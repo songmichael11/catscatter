d9e56148c01b360c0b18fc76bca9ddc04d7586f1
Michael Song <songmichael11@gmail.com>
2025-08-12 11:28:56 -0400

    feat:added secondary value that can be represented by color of marker

--- catscatter.py
+++ catscatter.py
@@ -1,20 +1,24 @@
 '''
     Created by @myrthings 2/4/20
+    Modified by @songmichael11 8/12/2025
 '''
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
-def catscatter(df,colx,coly,cols,color=['grey','black'],ratio=10,font='Helvetica',save=False,save_name='Default'):
+
+def catscatter(df,colx,coly,col_size,col_color,color=['grey','black'],ratio=10,font='Helvetica',save=False,save_name='Default'):
     '''
     Goal: This function create an scatter plot for categorical variables. It's useful to compare two lists with elements in common.
     Input:
         - df: required. pandas DataFrame with at least two columns with categorical variables you want to relate, and the value of both (if it's just an adjacent matrix write 1)
         - colx: required. The name of the column to display horizontaly
         - coly: required. The name of the column to display vertically
-        - cols: required. The name of the column with the value between the two variables
-        - color: optional. Colors to display in the visualization, the length can be two or three. The two first are the colors for the lines in the matrix, the last one the font color and markers color.
-            default ['grey','black']
+        - col_size: required. The name of the column with the value between the two variables, dictates size of the markers.
+        - col_color: required. The name of the column with the value between the two variables, dictates color of the markers.
+        - color: optional. A color to make gradient for colors of the markers.
+            default ['grey','black']. The first color is used for the background lines, the second for the markers.
         - ratio: optional. A ratio for controlling the relative size of the markers.
             default 10
         - font: optional. The font for the ticks on the matrix.
@@ -33,8 +37,7 @@ def catscatter(df,colx,coly,cols,color=['grey','black'],ratio=10,font='Helvetica
     # Apply the encoding
     df[colx]=df[colx].apply(lambda x: colx_codes[x])
     df[coly]=df[coly].apply(lambda x: coly_codes[x])
-    
-    
+
     # Prepare the aspect of the plot
     plt.rcParams['xtick.bottom'] = plt.rcParams['xtick.labelbottom'] = False
     plt.rcParams['xtick.top'] = plt.rcParams['xtick.labeltop'] = True
@@ -50,12 +53,21 @@ def catscatter(df,colx,coly,cols,color=['grey','black'],ratio=10,font='Helvetica
     for num in range(len(colx_codes)):
         plt.vlines(num,-1,len(coly_codes)+1,linestyle='dashed',linewidth=1,color=color[num%2],alpha=0.5)
         
-    # Plot the scatter plot with the numbers
-    plt.scatter(df[colx],
-               df[coly],
-               s=df[cols]*ratio,
-               zorder=2,
-               color=color[-1])
+    base_cmap = plt.cm.Blues  # all shades of blue
+
+    norm = mpl.colors.Normalize(vmin=df[col_color].min(), vmax=df[col_color].max())
+
+    plt.scatter(
+        df[colx],
+        df[coly],
+        s=df[col_size] * ratio,
+        zorder=2,
+        c=df[col_color],
+        cmap=base_cmap,
+        norm=norm
+    )
+
+    plt.colorbar(label=col_color)
     
     # Change the ticks numbers to categories and limit them
     plt.xticks(ticks=list(colx_codes.values()),labels=colx_codes.keys(),rotation=90)
